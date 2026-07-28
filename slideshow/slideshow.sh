c4b8f6e7fd855690e4bc2587b557871482d1f4d8
Luis Navarro <93lenm@gmail.com>
2026-07-27 20:08:29 -0600

    Reshuffle slideshow playlist each loop instead of relying on mpv --shuffle

--- slideshow/slideshow.sh
+++ slideshow/slideshow.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+export DISPLAY=:0
+export XAUTHORITY=/home/portrait/.Xauthority
 
 xset s off
 xset -dpms
 xset s noblank
 
-mpv --fullscreen \
-    --loop-playlist=inf \
-    --image-display-duration=6 \
-    --no-osc \
-    --shuffle \
-    ~/Pictures/
+PLAYLIST=/tmp/slideshow_playlist.txt
+MEDIA_DIR=~/Pictures
+
+while true; do
+    find "$MEDIA_DIR" -type f -not -name '.*' | shuf > "$PLAYLIST"
+
+    mpv --fullscreen \
+        --vo=x11 \
+        --no-osc \
+        --image-display-duration=6 \
+        --playlist="$PLAYLIST"
+done
